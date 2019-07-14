d66b6af3f74a9af87c781063fbf632892ac9fc26
Modrarus <modrarus@outlook.com>
2019-07-14 15:22:03 +0500

    Исправлено отсутствие анотации о представлении параметров запроса услуги из тела запроса

--- src/main/java/org/modrarus/govservice/controller/GovServiceController.java
+++ src/main/java/org/modrarus/govservice/controller/GovServiceController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -67,7 +68,7 @@ public class GovServiceController {
 	 */
 	@PostMapping("/api/{serviceId}/request")
 	public void requestService(@PathVariable("serviceId") final String _id,
-			final GovServiceRequestData _requestData) {
+			@RequestBody final GovServiceRequestData _requestData) {
 		GovService service = serviceRepository.findById(_id)
 				.orElseThrow(() -> new GovServiceNotExistException(_id));
 		
