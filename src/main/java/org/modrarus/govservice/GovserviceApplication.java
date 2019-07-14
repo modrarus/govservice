743771c781556de7b022c7409c07a2bd87e50c97
Modrarus <modrarus@outlook.com>
2019-07-14 11:46:52 +0500

    Инициализация проекта на Spring Boot

---
+++ src/main/java/org/modrarus/govservice/GovserviceApplication.java
@@ -0,0 +1,13 @@
+package org.modrarus.govservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovserviceApplication.class, args);
+	}
+
+}
